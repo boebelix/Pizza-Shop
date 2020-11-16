4c4ef4580b73341718cc87851ab63c22280dfaf1
CaptainN3ro <nicolas-schwarz@outlook.de>
2020-11-16 23:13:38 +0100

    Created functions.js, style.css and index.html, login, signup and overview div containers are already designed, the ordering div is also already designed and a few js code has been added

---
+++ apps/webApp/src/functions.js
@@ -0,0 +1,23 @@
+function showLogin() {
+    document.getElementById("overlay_login").style.display = "block";
+}
+
+function showSignup() {
+    document.getElementById("overlay_signup").style.display = "block";
+}
+
+function hideLogin() {
+    document.getElementById("overlay_login").style.display = "none";
+}
+
+function hideSignup() {
+    document.getElementById("overlay_signup").style.display = "none";
+}
+
+function showMain() {
+    document.getElementById("main_page").style.display = "block";
+}
+
+function hideMain() {
+    document.getElementById("main_page").style.display = "none";
+}
