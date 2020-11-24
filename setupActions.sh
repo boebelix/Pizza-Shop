0952673245ca26e5d81624488860bac84bf68528
toxicvipa <robin.tretter@gmail.com>
2020-11-24 15:08:07 +0100

    added e flag to sed

--- setupActions.sh
+++ setupActions.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed "s/%flutterSdk%/$(echo $FLUTTER_HOME)/g" local.properties.example > local.properties
+sed -e "s/%flutterSdk%/$(echo $FLUTTER_HOME)/g" local.properties.example > local.properties
