96beab8391f1bbf48f175aca409ecf5df370e550
nisc0055 <nisc0055@stud.hs-kl.de>
2020-11-24 20:39:50 +0100

    Added a few validation logic

--- apps/webApp/src/js/controller/signup.js
+++ apps/webApp/src/js/controller/signup.js
@@ -1,4 +1,54 @@
 const initSignup = () => {
-    document.getElementById("register_submit_button").addEventListener("click", () => setState("menu_state"));
+    document.getElementById("register_submit_button").addEventListener("click", () => submitData());
     document.getElementById("register_back_button").addEventListener("click", () => setState("login_state"));
 }
+
+const submitData = async () => {
+    let accepted = false;
+    let email = emailValidator(document.getElementById("email_signup").value);
+    let name = nameValidator(document.getElementById("name_signup").value);
+    let prename = prenameValidator(document.getElementById("prename_signup").value);
+    let username = usernameValidator(document.getElementById("username_signup").value);
+    let password = passwordValidator(document.getElementById("password_signup").value,document.getElementById("password_signup_repeat").value);
+    let street = streetValidator(document.getElementById("street_signup").value);
+    let number = numberValidator(document.getElementById("number_signup").value);
+    let postalcode = postalcodeValidator(document.getElementById("postalcode_signup").value);
+    let city = cityValidator(document.getElementById("city_signup").value);
+    let country = countryValidator(document.getElementById("country_signup").value);
+
+    if (email && name &&prename && username && password && street
+        && number && postalcode && city && country){
+            accepted = true;
+        }
+
+    if(accepted != false){
+    let data={"email" : document.getElementById("email_signup").value,
+    "lastName" : document.getElementById("name_signup").value,
+    "firstName" : document.getElementById("prename_signup").value,
+    "username" : document.getElementById("username_signup").value,
+    "password" : document.getElementById("password_signup").value,
+    "street" : document.getElementById("street_signup").value,
+    "nr" : document.getElementById("number_signup").value,
+    "postalCode" : document.getElementById("postalcode_signup").value,
+    "city" : document.getElementById("city_signup").value,
+    "country" : document.getElementById("country_signup").value};
+    }
+    let err = postData(data);
+    console.log(err);
+}
+
+const postData = async (url = '', data) => {
+    let errorcode;
+    const response = await fetch(url, {
+      method: 'POST',
+      
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    }).catch(err => {errorcode=err});
+    return errrorcode;
+  }
+
+
---
+++ apps/webApp/src/js/controller/validator.js
@@ -0,0 +1,117 @@
+const emailValidator = (value) => {
+    if(value.includes("@") & value.length>=7){
+        document.getElementById("error_email").style.display="none";
+        document.getElementById("email_signup").classList.remove("btn_error");
+        return true;
+    }else{
+        document.getElementById("email_signup").classList.add("btn_error");
+        document.getElementById("error_email").style.display="block";
+        return false;
+    }
+}
+
+const nameValidator = (value) => {
+    if(value.length == 0){
+        document.getElementById("name_signup").classList.add("btn_error");
+        return false;
+    }else{
+        document.getElementById("name_signup").classList.remove("btn_error");
+        return true;
+    }
+}
+
+const prenameValidator = (value) => {
+    if(value.length == 0){
+        document.getElementById("prename_signup").classList.add("btn_error");
+        return false;
+    }else{
+        document.getElementById("prename_signup").classList.remove("btn_error");
+        return true;
+    }
+}
+
+const usernameValidator = (value) => {
+    if(value.length == 0){
+        document.getElementById("username_signup").classList.add("btn_error");
+        return false;
+    }else{
+        document.getElementById("username_signup").classList.remove("btn_error");
+        return true;
+    }
+}
+
+const passwordValidator = (value,repeat) => {
+    let ret = true;
+
+    if(value.length == 0){
+        document.getElementById("password_signup").classList.add("btn_error");
+        ret = false;
+    }
+    if(repeat.length == 0){
+        document.getElementById("password_signup_repeat").classList.add("btn_error");
+        ret = false;
+    }
+    if(value != repeat){
+        document.getElementById("password_signup").classList.add("btn_error");
+        document.getElementById("password_signup_repeat").classList.add("btn_error");
+        document.getElementById("error_password").style.display="block";
+        ret=false;
+    }
+
+    if(ret == true){
+        document.getElementById("password_signup").classList.remove("btn_error");
+        document.getElementById("password_signup_repeat").classList.remove("btn_error");
+        document.getElementById("error_password").style.display="none";
+    }
+    return ret;
+}
+
+const streetValidator = (value) => {
+    if(value.length == 0){
+        document.getElementById("street_signup").classList.add("btn_error");
+        return false;
+    }else{
+        document.getElementById("street_signup").classList.remove("btn_error");
+        return true;
+    }
+}
+
+const numberValidator = (value) => {
+    if(value.length == 0){
+        document.getElementById("number_signup").classList.add("btn_error");
+        return false;
+    }else{
+        document.getElementById("number_signup").classList.remove("btn_error");
+        return true;
+    }
+}
+
+const postalcodeValidator = (value) => {
+    if(value.length == 0){
+        document.getElementById("postalcode_signup").classList.add("btn_error");
+        return false;
+    }else{
+        document.getElementById("postalcode_signup").classList.remove("btn_error");
+        return true;
+    }
+}
+
+const cityValidator = (value) => {
+    if(value.length == 0){
+        document.getElementById("city_signup").classList.add("btn_error");
+        return false;
+    }else{
+        document.getElementById("city_signup").classList.remove("btn_error");
+        return true;
+    }
+}
+
+const countryValidator = (value) => {
+    if(value.length == 0){
+        document.getElementById("country_signup").classList.add("btn_error");
+        return false;
+    }else{
+        document.getElementById("country_signup").classList.remove("btn_error");
+        return true;
+    }
+}
