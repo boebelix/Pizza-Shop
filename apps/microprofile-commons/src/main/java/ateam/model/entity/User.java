bf053fa8f8347658303966bb9d5ef2460296ce54
alex9849 <alexander@liggesmeyer.net>
2020-11-28 13:45:00 +0100

    Fixed validator-messages and regexes for user-model

--- apps/microprofile-commons/src/main/java/ateam/model/entity/User.java
+++ apps/microprofile-commons/src/main/java/ateam/model/entity/User.java
@@ -9,44 +9,44 @@ public class User {
 
 	private int userId;
 
-	@Validator.Required
-	@Validator.Min(1)
+	@Validator.Required(errorMessage = "Nutzername benötigt!")
+	@Validator.Min(value = 1, errorMessage = "Das Feld \"Nutzername\" darf nicht leer sein!")
 	private String username;
 
-	@Validator.Required
-	@Validator.Min(1)
+	@Validator.Required(errorMessage = "Vorname benötigt!")
+	@Validator.Min(value = 1, errorMessage = "Das Feld \"Vorname\" darf nicht leer sein!")
 	private String firstName;
 
-	@Validator.Required
-	@Validator.Min(1)
+	@Validator.Required(errorMessage = "Nachname benötigt!")
+	@Validator.Min(value = 1, errorMessage = "Das Feld \"Nachname\" darf nicht leer sein!")
 	private String lastName;
 
-	@Validator.Required
-	@Validator.Regex(regex = "^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$", errorMessage = "Email benötigt!")
+	@Validator.Required(errorMessage = "Email benötigt!")
+	@Validator.Regex(regex = "^(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$", errorMessage = "Gültige Email benötigt!")
 	private String email;
 
-	@Validator.Required
-	@Validator.Min(1)
+	@Validator.Required(errorMessage = "Passwort benötigt!")
+	@Validator.Min(value = 1, errorMessage = "Das Feld \"Passwort\" darf nicht leer sein!")
 	private String password;
 
-	@Validator.Required
+	@Validator.Required(errorMessage = "Postleitzahl benötigt!")
 	@Validator.Regex(regex = "^[0-9]{5}$", errorMessage = "Postleitzahl muss aus 5 Zahlen bestehen!")
 	private String postalCode;
 
-	@Validator.Required
-	@Validator.Min(1)
+	@Validator.Required(errorMessage = "Straße benötigt!")
+	@Validator.Min(value = 1, errorMessage = "Das Feld \"Straße\" darf nicht leer sein!")
 	private String street;
 
 	@Validator.Required(errorMessage = "Hausnummer benötigt!")
-	@Validator.Regex(regex = "^[0-9]+$", errorMessage = "HausNUMMER benötigt!")
+	@Validator.Regex(regex = "^[0-9]+(( )?[A-Za-z])?$", errorMessage = "Hausnummer ungültig!")
 	private String number;
 
-	@Validator.Required
-	@Validator.Min(1)
+	@Validator.Required(errorMessage = "Stadt benötigt!")
+	@Validator.Min(value = 1, errorMessage = "Das Feld \"Stadt\" darf nicht leer sein!")
 	private String city;
 
-	@Validator.Required
-	@Validator.Min(1)
+	@Validator.Required(errorMessage = "Land benötigt!")
+	@Validator.Min(value = 1, errorMessage = "Das Feld \"Land\" darf nicht leer sein!")
 	private String country;
 
 	private Date createAt;
