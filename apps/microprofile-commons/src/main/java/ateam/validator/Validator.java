e1fc89e9569e87ba801e6f7ca2d1738df59e19f2
alex9849 <alexander@liggesmeyer.net>
2020-11-28 13:55:38 +0100

    Added recursive object validation

--- apps/microprofile-commons/src/main/java/ateam/validator/Validator.java
+++ apps/microprofile-commons/src/main/java/ateam/validator/Validator.java
@@ -5,6 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.reflect.Field;
+import java.util.HashSet;
 
 public class Validator {
 
@@ -39,32 +40,43 @@ public class Validator {
 	}
 
 	public static void validate(Object object) {
+		validate(object, new HashSet<>());
+	}
+
+	private static void validate(Object object, HashSet<Object> seenObjects) {
 		for(Field field : object.getClass().getDeclaredFields()) {
 			try {
 				field.setAccessible(true);
+				if(field.get(object) != null && seenObjects.contains(field.get(object))) {
+					continue;
+				}
 				if(field.isAnnotationPresent(Required.class) && field.get(object) == null) {
 					throw new ValidationException(field.getAnnotation(Required.class).errorMessage().replace("%fieldname%", field.getName()));
 				}
-				if(field.isAnnotationPresent(Regex.class) && field.get(object) != null) {
-					String string = String.valueOf(field.get(object));
-					String regex = field.getAnnotation(Regex.class).regex();
-					if(!string.matches(regex)) {
-						throw new ValidationException(field.getAnnotation(Regex.class).errorMessage().replace("%fieldname%", field.getName()));
+				if(field.get(object) != null) {
+					if(field.isAnnotationPresent(Regex.class)) {
+						String string = String.valueOf(field.get(object));
+						String regex = field.getAnnotation(Regex.class).regex();
+						if(!string.matches(regex)) {
+							throw new ValidationException(field.getAnnotation(Regex.class).errorMessage().replace("%fieldname%", field.getName()));
+						}
 					}
-				}
-				if(field.isAnnotationPresent(Min.class) && field.get(object) != null) {
-					long min = field.getAnnotation(Min.class).value();
-					if(checkIsSmallerThen(field.get(object), min)) {
-						throw new ValidationException(field.getAnnotation(Min.class).errorMessage()
-							.replace("%fieldname%", field.getName()).replace("%value%", String.valueOf(min)));
+					if(field.isAnnotationPresent(Min.class)) {
+						long min = field.getAnnotation(Min.class).value();
+						if(checkIsSmallerThen(field.get(object), min)) {
+							throw new ValidationException(field.getAnnotation(Min.class).errorMessage()
+								.replace("%fieldname%", field.getName()).replace("%value%", String.valueOf(min)));
+						}
 					}
-				}
-				if(field.isAnnotationPresent(Max.class) && field.get(object) != null) {
-					long max = field.getAnnotation(Max.class).value();
-					if(checkIsSmallerThen(max, field.get(object))) {
-						throw new ValidationException(field.getAnnotation(Max.class).errorMessage()
-							.replace("%fieldname%", field.getName()).replace("%value%", String.valueOf(max)));
+					if(field.isAnnotationPresent(Max.class)) {
+						long max = field.getAnnotation(Max.class).value();
+						if(checkIsSmallerThen(max, field.get(object))) {
+							throw new ValidationException(field.getAnnotation(Max.class).errorMessage()
+								.replace("%fieldname%", field.getName()).replace("%value%", String.valueOf(max)));
+						}
 					}
+					seenObjects.add(object);
+					validate(field.get(object), seenObjects);
 				}
 				field.setAccessible(false);
 			} catch (IllegalAccessException e) {
