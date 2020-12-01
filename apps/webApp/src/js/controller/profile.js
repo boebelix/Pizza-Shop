2405fdc6e62c71a013f3554556c094429ce5e5f7
nisc0055 <nisc0055@stud.hs-kl.de>
2020-12-01 23:46:53 +0100

    Added password change - untested - and general method for updating things

--- apps/webApp/src/js/controller/profile.js
+++ apps/webApp/src/js/controller/profile.js
@@ -1,6 +1,51 @@
 const initProfile = () => {
+	document.getElementById("submitted_profile_change").innerHTML="";
+	document.getElementById("error_profile_change").innerHTML="";
 	document.getElementById("profile_back_button").addEventListener("click", () => setState("menu_state"));
 	document.getElementById("logout_button").addEventListener("click", () => logout());
+	document.getElementById("passwd_change").addEventListener("click", () => changePasswd());
+}
+
+const changePasswd = async () => {
+	const newPasswd = document.getElementById("password_new_profile");
+	const newPasswdRepeat = document.getElementById("password_new_profile_repeat");
+	let error = document.getElementById("error_profile_change");
+	let submitted = document.getElementById("submitted_profile_change");
+	submitted.innerHTML="";
+	error.innerHTML="";
+
+	if(!passwordValidator(newPasswd.id, newPasswdRepeat.id)){
+		console.log(newPasswd.value+" "+newPasswdRepeat.value);
+		error.innerHTML = "Passwörter stimmen nicht überein";
+	}else{
+		const data = {"password": newPasswd.value};
+		let response = await updateData('http://localhost:9080/user/'+USER_JSON.username, data);
+
+		if (response.status != RESPONSE_OK){
+			error.innerHTML = response.message;
+		}else{
+			submitted.innerHTML = "Passwort erfolgreich geändert";
+			error.innerHTML = "";
+		}
+	}
+}
+
+const updateData = async (url, data) => {
+	const output = await fetch(url, {
+		method: 'PUT',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': getCookie("uuid")
+		},
+		body: JSON.stringify(data)
+	});
+	const status = output.status;
+	if (status == RESPONSE_INTERNAL_SERVER_ERROR) {
+		return {"message": "Serverfehler", "status": RESPONSE_INTERNAL_SERVER_ERROR};
+	}
+	let json = await output.json();
+	json.status = status;
+	return json;
 }
 
 const logout = () => {
