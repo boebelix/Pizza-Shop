3d257b1a2d99303a377f8c5697d829b514c2cd24
Saarli <mdmaurice.didion@gmail.com>
2020-12-07 12:29:24 +0100

    Stub für production und erste Rest Schnittstelle

---
+++ apps/pizzaProduction/src/main/java/ateam/endpoint/ProductionEndpoint.java
@@ -0,0 +1,44 @@
+package ateam.endpoint;
+
+import ateam.exceptionmapper.UnknownEntityExceptionMapper;
+import ateam.exceptionmapper.ValidationExceptionMapper;
+import ateam.model.OrderProducer;
+import ateam.model.entity.Order;
+import ateam.model.entity.User;
+import ateam.model.exception.UnauthorizedException;
+import ateam.model.exception.UnknownEntityException;
+import ateam.model.request.LoginData;
+import ateam.model.response.LoginResponse;
+import ateam.validator.Validator;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.UUID;
+
+@Path("/production")
+@RegisterProvider(ValidationExceptionMapper.class)
+@RegisterProvider(UnknownEntityExceptionMapper.class)
+@Singleton
+public class ProductionEndpoint {
+
+	@Inject
+	OrderProducer producer;
+
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response produceOrder(@RequestBody(required = true)Order order) throws UnauthorizedException, UnknownEntityException {
+		Validator.validate(order);
+		producer.produceOrder(order);
+
+		return Response.status(Response.Status.CREATED.getStatusCode()).build();
+	}
+}
---
+++ apps/pizzaProduction/src/main/java/ateam/model/OrderProducer.java
@@ -0,0 +1,15 @@
+package ateam.model;
+
+import ateam.model.entity.Order;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class OrderProducer {
+
+	public void produceOrder(Order order)
+	{
+
+	}
+}
+
