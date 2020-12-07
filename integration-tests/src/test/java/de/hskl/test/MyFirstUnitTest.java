c2954a1dd3e2624e18accb20f855d6885703f2b8
Julian Paulus <julianpaulus704@gmail.com>
2020-12-07 22:16:14 +0100

    added integrationTest gradle setup

---
+++ integration-tests/src/test/java/de/hskl/test/MyFirstUnitTest.java
@@ -0,0 +1,14 @@
+package de.hskl.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MyFirstUnitTest {
+
+	@Test
+	void addition() {
+		assertEquals(2, 2);
+	}
+
+}
