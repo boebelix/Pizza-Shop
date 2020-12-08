caaa80e84bed843894bf0223a546df48e9bd1c74
Julian Paulus <julianpaulus704@gmail.com>
2020-12-08 14:36:23 +0100

    test application health before integration test startup

--- integration-tests/src/test/java/de/hskl/test/MyFirstUnitTest.java
+++ integration-tests/src/test/java/de/hskl/test/MyFirstUnitTest.java
@@ -1,14 +1,48 @@
 package de.hskl.test;
 
+import ateam.model.entity.User;
+import ateam.user.endpoints.UserEndpoint;
+import de.hskl.test.util.JsonBProvider;
+import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MyFirstUnitTest {
 
+	private static UserEndpoint userEndpoint;
+
+	@BeforeAll
+	public static void setupClass() {
+		List<Class<?>> providers = new ArrayList<>();
+		//providers.add(JsonBindingProvider.class); // trivial implementation of a JAX-RS MessageBodyReader/Writer
+		providers.add(JsonBProvider.class);
+		String appPath = "http://localhost:9080/";
+		userEndpoint = JAXRSClientFactory.create(appPath, UserEndpoint.class, providers);
+	}
+
 	@Test
-	void addition() {
-		assertEquals(2, 2);
+	void addition() throws InterruptedException {
+
+		TimeUnit.SECONDS.sleep(30);
+
+		User toCreate = new User();
+		toCreate.setCity("Zweibrücken");
+		toCreate.setCountry("Deutschland");
+		toCreate.setEmail("test@stud.hs-kl.de");
+		toCreate.setNumber("42");
+		toCreate.setStreet("Amerikastraße");
+		toCreate.setPostalCode("66482");
+		toCreate.setFirstName("Max");
+		toCreate.setLastName("Mustermann");
+		toCreate.setPassword("123456789#!TesT");
+
+		userEndpoint.createUser(toCreate);
 	}
 
 }
