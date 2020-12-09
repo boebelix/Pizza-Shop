2f835b6a77b448e7856ba6f041903583bc855cda
boebelix <sboebel@t-online.de>
2020-12-09 18:13:30 +0100

    Add navigaton bottom bar and delete depracated examble code from main.dart. Also refectored main.dart now only routes and global design is find in it

--- apps/mobileApp/lib/main.dart
+++ apps/mobileApp/lib/main.dart
@@ -1,68 +1,54 @@
-import 'package:app/widgets/navmenu_item.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import 'models/meals.dart';
+import 'screens/meal_creation_screen.dart';
+import 'screens/meal_detail_screen.dart';
+import 'widgets/nav_bottom.dart';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+void main() => runApp(
+      ChangeNotifierProvider(
+        create: (_) => Meals(),
+        child: App(),
+      ),
+    );
+
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Non Stop Pizza',
       theme: ThemeData(
+        primarySwatch: Colors.amber,
+        accentColor: Colors.amber,
+        // errorColor: Colors.red,
+        fontFamily: 'Quicksand',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              title: TextStyle(
+                fontFamily: 'OpenSans',
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+              button: TextStyle(color: Colors.white),
+            ),
 
-        primarySwatch: Colors.orange,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Non Stop Pizza'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Container(
-        decoration: new BoxDecoration(
-          image: new DecorationImage(
-            image: new AssetImage("images/Background_Login.jpg"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Flexible(child: NavMenuItem(position: 1, headlineTxt: 'Bestellung',)),
-              Flexible(child: NavMenuItem(position: 2, headlineTxt: 'Bestellübersicht',)),
-              Flexible(child: NavMenuItem(position: 3, headlineTxt: 'Profil',)),
-            ],
-          ),
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+                title: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
         ),
       ),
+
+      initialRoute: NavBottom.routeName,
+      routes: {
+        NavBottom.routeName: (_) => NavBottom(),
+        MealCreationScreen.routeName: (_) => MealCreationScreen(),
+        MealDetailScreen.routeName: (_) => MealDetailScreen(),
+      },
     );
   }
 }
