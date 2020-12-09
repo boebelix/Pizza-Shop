492ddce88594739bd18fa67d679e25629b520e17
boebelix <sboebel@t-online.de>
2020-12-09 18:14:19 +0100

    Add navigation path for bottom navigation. Implement lecture examble in code base

--- apps/mobileApp/lib/screens/create_user_screen.dart
+++ apps/mobileApp/lib/screens/create_user_screen.dart
@@ -9,89 +9,85 @@ class CreateUserScreen extends StatelessWidget {
   final TextEditingController name = TextEditingController();
   final TextEditingController username = TextEditingController();
   final TextEditingController street = TextEditingController();
-  final TextEditingController suite = TextEditingController();
+  final TextEditingController number = TextEditingController();
   final TextEditingController city = TextEditingController();
   final TextEditingController zip = TextEditingController();
   final TextEditingController email = TextEditingController();
   final TextEditingController phone = TextEditingController();
-  final TextEditingController website = TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Create a new User"),
-      ),
-      body: Form(
-        key: _formKey,
-        child: ListView(
-          padding: const EdgeInsets.all(8),
-          children: [
-            TextFormField(
-              decoration: InputDecoration(labelText: "Full name"),
-              controller: name,
-              validator: (value) =>
-                  value?.isEmpty ?? true ? "Enter a valid name" : null,
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: "Username"),
-              controller: username,
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: "Street"),
-              controller: street,
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: "Suite"),
-              controller: suite,
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: "City"),
-              controller: city,
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: "Zip"),
-              controller: zip,
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: "Email"),
-              controller: email,
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: "Phone"),
-              controller: phone,
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: "Website"),
-              controller: website,
-            ),
-            SizedBox(height: 16),
-            RaisedButton(
-              child: Text("Submit"),
-              onPressed: () {
-                if (_formKey.currentState.validate()) {
-                  context.read<Users>().createUser(
-                        User(
-                          name: name.text,
-                          username: username.text,
-                          address: Address(
-                            city: city.text,
-                            street: street.text,
-                            suite: suite.text,
-                            zipcode: zip.text,
+    return Container(
+      child:  Form(
+          key: _formKey,
+          child: ListView(
+            padding: const EdgeInsets.all(8),
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: "Name"),
+                controller: name,
+                validator: (value) =>
+                    value?.isEmpty ?? true ? "Bitte Name eingeben" : null,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: "Nutzername"),
+                controller: username,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: "Straße"),
+                controller: street,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: "Hausnummer"),
+                controller: number,
+                keyboardType: TextInputType.number,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: "Stadt"),
+                controller: city,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: "Postleitzahl"),
+                controller: zip,
+                keyboardType: TextInputType.number,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: "E-Mail"),
+                controller: email,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: "Telefonnummer"),
+                controller: phone,
+                keyboardType: TextInputType.number,
+              ),
+
+              SizedBox(height: 16),
+              RaisedButton(
+                child: Text("Ändern"),
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    context.read<Users>().createUser(
+                          User(
+                            name: name.text,
+                            username: username.text,
+                            address: Address(
+                              city: city.text,
+                              street: street.text,
+                              suite: number.text,
+                              zipcode: zip.text,
+                            ),
+                            email: email.text,
+                            phone: phone.text,
                           ),
-                          email: email.text,
-                          phone: phone.text,
-                          website: website.text,
-                        ),
-                      );
-                  Navigator.pop(context);
-                }
-              },
-            ),
-          ],
+                        );
+                    Navigator.pop(context);
+                  }
+                },
+              ),
+            ],
+          ),
         ),
-      ),
     );
   }
 }
