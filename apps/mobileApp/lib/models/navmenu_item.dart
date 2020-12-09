bebfeec410b04c867a95c708fedb33f164bca605
boebelix <sboebel@t-online.de>
2020-12-09 18:04:55 +0100

    Delete deprecated elements

--- apps/mobileApp/lib/models/navmenu_item.dart
+++
@@ -1,9 +0,0 @@
-class NavmenuItem {
-  final String imagePath;
-  final String headlineText;
-  final String routePath;
-  final double color;
-
-  NavmenuItem({this.imagePath, this.headlineText, this.routePath, this.color});
-
-}
--- apps/mobileApp/lib/widgets/navmenu_list.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class NavmenuList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
