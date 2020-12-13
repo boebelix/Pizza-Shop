7ccaa1f330a3138289f6cdb7b30169fcf0d1a6b7
LucasReinhard <lucas-reinhard@gmx.net>
2020-12-13 19:23:38 +0100

    finished procurement tests

--- integration-tests/src/test/java/ateam/test/procurementService/ProcurementControllerTest.java
+++ integration-tests/src/test/java/ateam/test/procurementService/ProcurementControllerTest.java
@@ -1,5 +1,6 @@
 package ateam.test.procurementService;
 
+import ateam.client.procurement.ProcurementClient;
 import ateam.model.entity.ProcurementLog;
 import ateam.model.entity.ProcurementLogItem;
 import ateam.procurement.service.ProcurementController;
@@ -10,20 +11,21 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import javax.ws.rs.core.Response;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ProcurementControllerTest {
-	private static ProcurementController procurementController;
+	private static ProcurementClient procurementClient;
 
 	@BeforeAll
 	public static void setupClass() {
-		procurementController = TestUtils.setupClient(TestConstants.PROCUREMENT_SERVICE_URI, ProcurementController.class);
+		procurementClient = TestUtils.setupClient(TestConstants.PROCUREMENT_SERVICE_URI, ProcurementClient.class);
 	}
 
 	@Test
-	void createProcurementLog() throws LogServiceException {
+	void createProcurementLog() throws IOException {
 		ProcurementLog log = new ProcurementLog();
 		ArrayList<ProcurementLogItem> logItems = new ArrayList<>();
 		ProcurementLogItem logItem = new ProcurementLogItem();
@@ -34,7 +36,7 @@ public class ProcurementControllerTest {
 		logItems.add(logItem);
 		log.setItems(logItems);
 
-		Response response = procurementController.createProcurementLog(log);
+		Response response = procurementClient.createProcurementLog(log);
 
 		assertEquals(Response.Status.CREATED.getStatusCode(), response.getStatus());
 	}
--- integration-tests/src/test/java/ateam/test/util/TestConstants.java
+++ integration-tests/src/test/java/ateam/test/util/TestConstants.java
@@ -3,6 +3,7 @@ package ateam.test.util;
 public class TestConstants {
 
 	public final static String USER_SERVICE_URI = "http://localhost:9080/";
-	public final static String PROCUREMENT_SERVICE_URI= "http://localhost:9080/";
+	public final static String PROCUREMENT_SERVICE_URI= "http://localhost:9082/";
+	public final static String LOGISTICS_SERVICE_URI= "http://localhost:9081/";
 
 }
