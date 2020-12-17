e101c6a45bf82847caf849479e572a4ce6683f62
Julian Paulus <julianpaulus704@gmail.com>
2020-12-17 11:47:46 +0100

    fixed build error

--- integration-tests/src/test/java/ateam/test/pizzaProduction/ProductionTestUtils.java
+++ integration-tests/src/test/java/ateam/test/pizzaProduction/ProductionTestUtils.java
@@ -7,7 +7,7 @@ public class ProductionTestUtils {
 	public static ShopProductionItem createDefaultOrder() {
 		ShopProductionItem item=new ShopProductionItem();
 		Pizza testPizza=new Pizza();
-		testPizza.setID(1);
+		testPizza.setId(1);
 		testPizza.setOrderId(1);
 		testPizza.setSizeId(2);
 		Topping topping=new Topping(1,"Salami", 10,"Gramm");
