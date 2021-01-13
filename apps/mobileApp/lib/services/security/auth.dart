6b5a88be07f38c4eba57fb675d84c04a8960b227
boebelix <sboebel@t-online.de>
2021-01-13 19:55:31 +0100

    change name

---
+++ apps/mobileApp/lib/services/security/auth.dart
@@ -0,0 +1,38 @@
+import 'package:app/models/user.dart';
+
+class AuthService{
+  static AuthService _instance;
+
+  factory AuthService.instance() {
+    _instance ??= AuthService._private();
+    return _instance;
+  }
+
+  AuthService._private();
+
+
+  User _user;
+  String _uuid;
+
+
+  User get user => _user;
+
+  set user(User value) {
+    _user = value;
+  }
+
+  String get uuid => _uuid;
+
+  set uuid(String value) {
+    _uuid = value;
+  }
+
+  bool get isSignIn{
+    return _user != null && _uuid != null;
+  }
+
+  void logoutUser(){
+    _user = null;
+    _uuid = null;
+  }
+}
