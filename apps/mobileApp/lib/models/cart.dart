a0fe2524c0e19d4f2d42543b736448e72bb0044c
alex9849 <alexander@liggesmeyer.net>
2021-01-19 17:59:54 +0100

    Added cart view

--- apps/mobileApp/lib/models/cart.dart
+++ apps/mobileApp/lib/models/cart.dart
@@ -10,9 +10,63 @@ class Cart extends ChangeNotifier {
   * Bilder von
   * https://www.essen-und-trinken.de/pizza/73081-cstr-die-besten-pizza-rezepte
   * */
-  List<Pizza> _pizzas = [Pizza(
-    Size(id: 1, basePrice: 30, diameter: 10, doughAmount: 200)
-  ).addTopping(Topping(baseAmount: 10, id: 1, name: "Bugs", unit: "g"))];
+  List<Pizza> _pizzas = [
+    Pizza(Size(
+            id: 1,
+            basePrice: 30,
+            diameter: 10,
+            doughAmount: 200,
+            toppingFactor: 1.5,
+            toppingPrice: 2.5))
+        .addTopping(Topping(baseAmount: 10, id: 1, name: "Bugs", unit: "g"))
+        .addTopping(Topping(baseAmount: 10, id: 1, name: "Errors", unit: "g")),
+    Pizza(Size(
+            id: 1,
+            basePrice: 30,
+            diameter: 10,
+            doughAmount: 200,
+            toppingFactor: 1.5,
+            toppingPrice: 2.5))
+        .addTopping(Topping(baseAmount: 10, id: 1, name: "Bugs", unit: "g"))
+        .addTopping(Topping(baseAmount: 10, id: 1, name: "Errors", unit: "g")),
+    Pizza(Size(
+            id: 1,
+            basePrice: 30,
+            diameter: 10,
+            doughAmount: 200,
+            toppingFactor: 1.5,
+            toppingPrice: 2.5))
+        .addTopping(Topping(baseAmount: 10, id: 1, name: "Bugs", unit: "g"))
+        .addTopping(Topping(baseAmount: 10, id: 1, name: "Errors", unit: "g")),
+    Pizza(Size(
+            id: 1,
+            basePrice: 30,
+            diameter: 10,
+            doughAmount: 200,
+            toppingFactor: 1.5,
+            toppingPrice: 2.5))
+        .addTopping(Topping(baseAmount: 10, id: 1, name: "Bugs", unit: "g"))
+        .addTopping(Topping(baseAmount: 10, id: 1, name: "Errors", unit: "g"))
+        .addTopping(Topping(baseAmount: 10, id: 1, name: "Errors", unit: "g"))
+        .addTopping(Topping(baseAmount: 10, id: 1, name: "Errors", unit: "g"))
+        .addTopping(Topping(baseAmount: 10, id: 1, name: "Errors", unit: "g"))
+        .addTopping(Topping(baseAmount: 10, id: 1, name: "Errors", unit: "g"))
+        .addTopping(Topping(baseAmount: 10, id: 1, name: "Errors", unit: "g"))
+        .addTopping(Topping(baseAmount: 10, id: 1, name: "Errors", unit: "g"))
+        .addTopping(Topping(baseAmount: 10, id: 1, name: "Errors", unit: "g"))
+        .addTopping(Topping(baseAmount: 10, id: 1, name: "Errors", unit: "g"))
+        .addTopping(Topping(baseAmount: 10, id: 1, name: "Errors", unit: "g"))
+        .addTopping(Topping(baseAmount: 10, id: 1, name: "Errors", unit: "g")),
+    Pizza(Size(
+            id: 1,
+            basePrice: 30,
+            diameter: 10,
+            doughAmount: 200,
+            toppingFactor: 1.5,
+            toppingPrice: 2.5))
+        .addTopping(Topping(baseAmount: 10, id: 1, name: "Bugs", unit: "g"))
+        .addTopping(Topping(baseAmount: 10, id: 1, name: "Errors", unit: "g"))
+  ];
 
   UnmodifiableListView<Pizza> get pizzas => UnmodifiableListView(_pizzas);
 
--- apps/mobileApp/lib/widgets/pizza_cart_summary.dart
+++ apps/mobileApp/lib/widgets/pizza_cart_summary.dart
@@ -1,3 +1,4 @@
+
 import 'package:app/models/Pizza.dart';
 import 'package:flutter/material.dart';
 
@@ -6,7 +7,12 @@ class PizzaCartSummary extends StatelessWidget {
 
   const PizzaCartSummary({Key key, this.pizza}) : super(key: key);
 
-
+  double calcPrice() {
+    double price =
+        pizza.size.basePrice + pizza.toppings.length * pizza.size.toppingPrice;
+    //return (price * 10).round() / 10.0;
+    return price;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,22 +21,43 @@ class PizzaCartSummary extends StatelessWidget {
     return Container(
       child: ClipRRect(
         borderRadius: BorderRadius.circular(radius),
-        child: Card(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text("Pizza ${pizza.size.diameter}cm",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        child: Opacity(
+          opacity: 0.8,
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "Pizza ${pizza.size.diameter}cm",
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      Spacer(),
+                      Text(
+                        "${calcPrice()}€",
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    padding: const EdgeInsets.only(
+                        top: 8, left: 4, right: 4, bottom: 8),
+                    itemCount: pizza.toppings.length,
+                    itemBuilder: (context, index) => Text(
+                      "- ${pizza.toppings[index].name}",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    shrinkWrap: true,
+                  ),
+                ],
               ),
-              ListView.builder(
-                padding: const EdgeInsets.only(top: 8, left: 4, right: 4, bottom: 72),
-                itemCount: pizza.toppings.length,
-                itemBuilder: (context, index) => Text(
-                    "- ${pizza.toppings[index].name}",
-                ),
-                shrinkWrap: true,
-              ),
-            ],
+            ),
           ),
         ),
       ),
