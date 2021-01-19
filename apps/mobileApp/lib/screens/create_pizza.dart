f36cfe2679f5d6658452cb0726946de0148bbf71
Saarli <mdmaurice.didion@gmail.com>
2021-01-19 15:40:07 +0100

    Checkboxen ohne fremde lib werden aus Map erzeugt, derzeit noch mit statischen einträgen

--- apps/mobileApp/lib/screens/create_pizza.dart
+++ apps/mobileApp/lib/screens/create_pizza.dart
@@ -1,18 +1,25 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:grouped_buttons/grouped_buttons.dart';
 
 class CreatePizzaScreen extends StatefulWidget {
   static const String routeName = "/createPizzaScreen";
-
   @override
   _CreatePizzaScreenState createState() => _CreatePizzaScreenState();
 }
 
 class _CreatePizzaScreenState extends State<CreatePizzaScreen> {
-  bool _isChecked = true;
-  String _currText = '';
+  bool _isChecked = true;//Map mit CurrentText als key, value _isChecked
+  String _currText = 'Test';//
+  var _labelCheckedMap={'Schinken':false,'Salami':false};//new Map();
+
+
+  //_labelCheckedMap['Salami']=false;
+  //_labelCheckedMap['Schinken']=false;
 
   List<String> text = ["InduceSmile.com", "Flutter.io", "google.com"];
+
 
   @override
   Widget build(BuildContext context) {
@@ -33,24 +40,20 @@ class _CreatePizzaScreenState extends State<CreatePizzaScreen> {
           ),
           Expanded(
               child: Container(
-            height: 350.0,
-            child: CheckboxGroup(
-              labels: <String>[
-                "Sunday",
-                "Monday",
-                "Tuesday",
-                "Wednesday",
-                "Thursday",
-                "Friday",
-                "Saturday",
-              ],
-              disabled: [
-                "Wednesday",
-                "Friday"
-              ],
-              onChange: (bool isChecked, String label, int index) => print("isChecked: $isChecked   label: $label  index: $index"),
-              onSelected: (List<String> checked) => print("checked: ${checked.toString()}"),
-            ),
+              height: 350.0,
+                child:Column(
+                children:[
+                  for(String key in _labelCheckedMap.keys)CheckboxListTile(
+                    title:  Text(key),
+                    tristate: false,
+                    value: _labelCheckedMap[key],
+                    onChanged: (newValue) {
+                      setState(() {
+                        _labelCheckedMap.update(key, (value) => newValue);
+                      });
+                    },
+              //  <-- leading Checkbox
+              )]),
           )),
         ],
       ),
