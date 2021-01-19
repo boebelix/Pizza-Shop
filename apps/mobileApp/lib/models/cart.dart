555778ad0545023fdb2be89d876dfc244ee423a1
alex9849 <alexander@liggesmeyer.net>
2021-01-19 16:38:39 +0100

    Refactored cart

--- apps/mobileApp/lib/models/cart.dart
+++ apps/mobileApp/lib/models/cart.dart
@@ -1,30 +1,19 @@
 import 'dart:collection';
 
+import 'package:app/models/Pizza.dart';
 import 'package:flutter/foundation.dart';
-
-import 'meal.dart';
 
 class Cart extends ChangeNotifier {
   /*
   * Bilder von
   * https://www.essen-und-trinken.de/pizza/73081-cstr-die-besten-pizza-rezepte
   * */
-  List<Meal> _meals = [
-    Meal(
-      imagePath:
-          "https://image.essen-und-trinken.de/11915996/t/Gf/v8/w960/r1/-/267910--5613-.jpg",
-      name: "Erstelle dir deine eigene Pizza",
-      description: "Sei dein eigener Koch",
-      price: 10.0,
-      rating: 5.0,
-      numberOfRatings: 0,
-    ),
-  ];
+  List<Pizza> _pizzas = [];
 
-  UnmodifiableListView<Meal> get meals => UnmodifiableListView(_meals);
+  UnmodifiableListView<Pizza> get pizzas => UnmodifiableListView(_pizzas);
 
-  void add(Meal meal) {
-    _meals.add(meal);
+  void add(Pizza pizza) {
+    _pizzas.add(pizza);
     notifyListeners();
   }
 }
