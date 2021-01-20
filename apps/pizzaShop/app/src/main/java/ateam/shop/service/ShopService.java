3cbe6b50b8692ee3b1eb283ed6d51d55e7db57e5
alex9849 <alexander@liggesmeyer.net>
2021-01-20 18:42:10 +0100

    Some fixes

--- apps/pizzaShop/app/src/main/java/ateam/shop/service/ShopService.java
+++ apps/pizzaShop/app/src/main/java/ateam/shop/service/ShopService.java
@@ -1,8 +1,8 @@
 package ateam.shop.service;
 
+import ateam.client.production.ProductionClient;
 import ateam.model.entity.*;
 import ateam.shop.db.DBManager;
-import ateam.client.production.ProductionClient;
 import org.eclipse.microprofile.rest.client.RestClientBuilder;
 
 import javax.inject.Inject;
@@ -22,13 +22,17 @@ public class ShopService {
 	@Inject
 	private DBManager dbManager;
 
-	@Inject
-	private ProductionClient productionClient;
+	private final ProductionClient productionClient;
+
+	public ShopService() throws NamingException, URISyntaxException {
+		this.productionClient = RestClientBuilder
+			.newBuilder().baseUri(new URI(InitialContext.doLookup("PizzaProductionInternURI"))).build(ProductionClient.class);
+	}
 
 	public Order placeOrder(Order order, int userId) {
 		order.setOrderDate(new Date(new GregorianCalendar().getTime().getTime()));
 		Order created = dbManager.placeOrder(order);
-		ShopProductionItem prodItem = parseOrderToProductionItem(order, userId);
+		ShopProductionItem prodItem = parseOrderToProductionItem(created, userId);
 		try {
 			productionClient.produceOrder(prodItem);
 		} catch (IOException e) {
@@ -62,10 +66,5 @@ public class ShopService {
 		for (PizzaTopping dbTopping : toppings)
 			pr.addTopping(dbManager.getToppingById(dbTopping.getToppingId()));
 		return pr;
-	}
-
-	public ShopService() throws NamingException, URISyntaxException {
-		this.productionClient = RestClientBuilder
-			.newBuilder().baseUri(new URI(InitialContext.doLookup("PizzaProductionInternURI"))).build(ProductionClient.class);
 	}
 }
